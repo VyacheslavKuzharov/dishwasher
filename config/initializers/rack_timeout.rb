a842f499b4ed3085f941e8fdd80d670da61dfabe
Vyacheslav Kuzharov <vyacheslav.kuzharov@gmail.com>
2021-02-21 15:28:14 +0300

    change

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,1 @@
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 300, wait_timeout: 300
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, wait_timeout: 300
